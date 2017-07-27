c182f67d2663f89ab65eb40355b7fa45f4795f4a
Kamran Ali <kamali@expedia.com>
2017-07-28 00:14:51 +0530

    One to One with Eager FetchType
    Ingredient is one to one mapped with Unit of measure

--- src/main/java/in/kamranali/domain/Ingredient.java
+++ src/main/java/in/kamranali/domain/Ingredient.java
@@ -3,10 +3,12 @@ package in.kamranali.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
@@ -23,4 +25,7 @@ public class Ingredient {
 	
 	@ManyToOne // No cascading because if we delete an ingredient we don't want it to delete recipe. 
 	private Recipe recipe;
+	
+	@OneToOne(fetch = FetchType.EAGER) // One to One fetch type is already eager
+	private UnitOfMeasure unitOfmeasure;
 }
