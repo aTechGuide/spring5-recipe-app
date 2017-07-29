a97ab2fd616942960699f5763211f6a10f8e38bf
Kamran Ali <kamali@expedia.com>
2017-07-29 16:44:33 +0530

    Query Methods
    Added Query methods for Category and UOM

--- src/main/java/in/kamranali/repositories/CategoryRepository.java
+++ src/main/java/in/kamranali/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package in.kamranali.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import in.kamranali.domain.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+	Optional<Category> findByDescription(String description);
 }
