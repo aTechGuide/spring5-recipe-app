a0c7f179daa9723d9c4dc3a90fc698c866cbc927
Kamran Ali <kamali@expedia.com>
2017-07-29 16:01:16 +0530

    Many to Many
    Many to Many relationship between recipe and Category.
    Provided custom Join table

---
+++ src/main/java/in/kamranali/domain/Category.java
@@ -0,0 +1,25 @@
+package in.kamranali.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Get the ID from DB upon persistence 
+	private Long id;
+	
+	private String description;
+	
+	@ManyToMany(mappedBy = "categories") // "categories" is the property name in Recipe class
+	private Set<Recipe> recipes;
+}
