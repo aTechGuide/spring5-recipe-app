46236a1080755342f82f68368917ecb9e6a35934
Kamran Ali <kamali@expedia.com>
2017-08-09 23:17:00 +0530

    Added Recipe Form

--- src/main/java/in/kamranali/controllers/RecipeController.java
+++ src/main/java/in/kamranali/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package in.kamranali.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import in.kamranali.commands.RecipeCommand;
 import in.kamranali.services.RecipeService;
 
 @Controller
@@ -21,5 +24,20 @@ public class RecipeController {
 		
 		model.addAttribute("recipe", recipeService.findByID(new Long(id)));
 		return "recipe/show";
+	}
+	
+	@RequestMapping("/recipe/new")
+	public String newRecipe(Model model){
+		
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+		
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 }
